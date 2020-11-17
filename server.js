e34f6da94d3aeffb615007e3313d14526c87f9a8
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-11-17 23:16:42 +0800

    🎨 Formated server.js with ESlint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const server = express();
 
-server.get("/", (req, res) => {
-	res.send("Your bot is alive!");
+server.get('/', (req, res) => {
+	res.send('Your bot is alive!');
 });
 
 function keepAlive() {
 	server.listen(8080, () => {
-		console.log("Server is Ready!");
+		console.log('Server is Ready!');
 	});
 }
 
