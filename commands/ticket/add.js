c7ff79a70229d9825114d50224d3e33f7373d3e3
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-11-17 17:35:26 +0800

    📦 Added incorrect usage warnings

--- commands/ticket/add.js
+++ commands/ticket/add.js
@@ -6,9 +6,12 @@ module.exports = {
 	usage: 'add <member>',
 	userperms: ['ADMINISTRATOR'],
 	botperms: [],
-	run: async (client, message, args) => {
-		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]);
+	run: async (client, message, args, prefix) => {
 		if(message.channel.name.includes('ticket-')) {
+			const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]);
+			if(!member) {
+				return message.channel.send(`Incorrect Usage! Correct Usage:${prefix}add <member>`);
+			}
 			try{
 				message.channel.updateOverwrite(member.user, {
 					VIEW_CHANNEL: true,
