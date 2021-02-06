c85b85b8b8f55430573fe6bc6c8ff779fb0cb92d
numselli <58607232+numselli@users.noreply.github.com>
2021-02-05 20:48:21 -0800

    add some logging

--- commands/ticket/new.js
+++ commands/ticket/new.js
@@ -26,6 +26,10 @@ module.exports = {
 		}).then(async channel => {
 			message.reply(`you have successfully created a ticket! Please click on ${channel} to view your ticket.`);
 			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`${prefix}close\``);
+			let logchannel = message.guild.channels.cache.find(channel => channel.name === `ticket-logs`)
+			if(logchannel) {
+				logchannel.send(`Ticket ${message.author.id} created. Click the following to veiw <#${message.author.id}>`);
+			}
 		});
 	},
 };
