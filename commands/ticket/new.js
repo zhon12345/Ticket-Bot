de8de27c153683ca5f1b8d1d88d688b149ec3b01
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-07-29 08:48:06 +0800

    🔧 Fixed minor bugs

--- commands/ticket/new.js
+++ commands/ticket/new.js
@@ -8,7 +8,7 @@ module.exports = {
 	botperms: [],
 	run: async (client, message, args, prefix) => {
 		if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.id}`)) {
-			return message.reply('you already have a ticket, please close your exsisting ticket first before opening a new one!');
+			return message.reply('you already have a ticket, please close your existing ticket first before opening a new one!');
 		}
 
 		message.guild.channels.create(`ticket-${message.author.id}`, {
@@ -26,7 +26,7 @@ module.exports = {
 		}).then(async channel => {
 			message.reply(`you have successfully created a ticket! Please click on ${channel} to view your ticket.`);
 			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`${prefix}close\``);
-			let logchannel = message.guild.channels.cache.find(channel => channel.name === `ticket-logs`)
+			const logchannel = message.guild.channels.cache.find(channel => channel.name === 'ticket-logs');
 			if(logchannel) {
 				logchannel.send(`Ticket ${message.author.id} created. Click the following to veiw <#${channel.id}>`);
 			}
